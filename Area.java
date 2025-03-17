8e29a0214b32e0c78d807544d9b1e7d94c48ed52
NavaneethS13 <vvce22cseaiml0111@vvce.ac.in>
2025-03-17 15:28:54 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.Scanner;
 public class Area{
   public static void main(String [] args){
-    float pi=3.14;
+    double pi=3.14;
     Scanner sc=new Scanner(System.in);
     float r=sc.nextInt();
     
